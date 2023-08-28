fa35357c6af343dc48b26e8567d70ee80557fbf3
edriball121 <53952759+edriball121@users.noreply.github.com>
2023-08-28 17:26:33 -0500

    Se crea el crud de granjero
    Se crea el crud de granjero correctamente

--- src/app/components/granjero/granjero.component.ts
+++ src/app/components/granjero/granjero.component.ts
@@ -1,15 +1,134 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { GranjeroService } from './../../services/granjero/granjero.service';
+import { ToastrService } from 'ngx-toastr';
+import { VerifyToken } from './../../services/verify-token.service';
 
 @Component({
   selector: 'app-granjero',
   templateUrl: './granjero.component.html',
-  styleUrls: ['./granjero.component.css']
+  styleUrls: [ './granjero.component.css' ]
 })
 export class GranjeroComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  form: FormGroup;
+  Farmers: any;
+  TituloAccion = 'Agregar';
+  gra_cedula: string | undefined;
+  rol!: string;
+  constructor(
+    private fb: FormBuilder,
+    private FarmerService: GranjeroService,
+    private toastr: ToastrService,
+    private verifyToken: VerifyToken,
+  ) {
+    //restricciones del formulario
+    this.form = this.fb.group({
+      gra_cedula: [ '', [ Validators.required, Validators.minLength(8), Validators.maxLength(11) ] ],
+      gra_nombre: [ '', Validators.required ],
+      gra_apellido: [ '', Validators.required ],
+      gra_password: [ '', Validators.required ],
+      gra_telefono: [ '', [ Validators.required, Validators.minLength(8), Validators.maxLength(11) ] ],
+      gra_email: [ '', [ Validators.required, Validators.email ] ],
+      gra_direccion: [ '', Validators.required ],
+      gra_fecha_nacimiento: [ '', Validators.required ],
+      gra_fecha_creacion: [ ''],
+      gra_perfil_laboral: [ '', Validators.required ],
+      rol: [ ''],
+    })
   }
 
+  ngOnInit(): void {
+    //listar granjeros
+    this.FarmerService.getFarmer().subscribe(respuesta => {
+      this.Farmers = respuesta;
+    });
+    //verificar rol
+    this.rol = this.verifyToken.obtenerRol();
+  }
+
+  //Agregar granjero
+  public addFarmer() {
+    //formatear la fecha
+    const fechaActualUtc: Date = new Date();
+    const fechaFormateada: string = fechaActualUtc.toISOString().split('T')[ 0 ];
+    const getRol = 'farmer';
+    //Capturar los valores del formulario
+    const Farmers: any = {
+      gra_cedula: this.form.get('gra_cedula')?.value.toString(),
+      gra_nombre: this.form.get('gra_nombre')?.value,
+      gra_apellido: this.form.get('gra_apellido')?.value,
+      gra_password: this.form.get('gra_password')?.value,
+      gra_telefono: this.form.get('gra_telefono')?.value.toString(),
+      gra_email: this.form.get('gra_email')?.value,
+      gra_direccion: this.form.get('gra_direccion')?.value,
+      gra_fecha_nacimiento: this.form.get('gra_fecha_nacimiento')?.value,
+      gra_perfil_laboral: this.form.get('gra_perfil_laboral')?.value,
+      gra_fecha_creacion: fechaFormateada,
+      rol: getRol
+    }
+    if (this.gra_cedula == undefined) {
+      Farmers.gra_cedula == undefined;
+      //Agregar granjero
+      this.FarmerService.addFarmer(Farmers).subscribe(respuesta => {
+        this.ngOnInit();
+        this.form.reset();
+        this.toastr.success('Se agrego el Granjero correctamente', 'Mensaje', {
+          positionClass: 'toast-top-center',
+          closeButton: true,
+          timeOut: 5000,
+          progressBar: true
+        });
+      });
+    } else {
+      this.FarmerService.editFarmer(this.gra_cedula, Farmers).subscribe(respuesta => {
+        this.TituloAccion = 'agregar';
+        this.form.reset();
+        this.ngOnInit();
+        this.gra_cedula = undefined;
+        this.toastr.success('Se edito el administrador correctamente', 'Mensaje', {
+          positionClass: 'toast-top-center',
+          closeButton: true,
+          timeOut: 5000,
+          progressBar: true
+        });
+      });
+    }
+  }
+  //Obtener los datos del granjero a editar
+  selectFarmer(farmer: any) {
+    this.TituloAccion = 'Editar';
+    this.gra_cedula = farmer.gra_cedula;
+    this.form.patchValue({
+      gra_cedula: farmer.gra_cedula,
+      gra_nombre: farmer.gra_nombre,
+      gra_apellido: farmer.gra_apellido,
+      gra_telefono: farmer.gra_telefono,
+      gra_email: farmer.gra_email,
+      gra_direccion: farmer.gra_direccion,
+      gra_fecha_nacimiento: farmer.gra_fecha_nacimiento,
+      gra_perfil_laboral: farmer.gra_perfil_laboral,
+      rol: farmer.rol,
+    });
+    this.toastr.info('Se cargaron los datos correctamente', 'Mensaje', {
+      positionClass: 'toast-top-center',
+      closeButton: true,
+      timeOut: 5000,
+      progressBar: true
+    });
+  }
+  //Eliminar granjero
+  deleteFarmer(gra_cedula: any, iControl: any) {
+    if (window.confirm('¿Esta seguro que desea borrar el registro?')) {
+      this.FarmerService.deleteFarmer(gra_cedula).subscribe((respuesta) => {
+        this.Farmers.splice(iControl, 1);
+        this.toastr.error('Se elimino el registro correctamente', 'Mensaje', {
+          positionClass: 'toast-top-center',
+          closeButton: true,
+          timeOut: 5000,
+          progressBar: true
+        });
+      })
+    }
+  }
 }
--- src/app/services/granjero/granjero.service.ts
+++ src/app/services/granjero/granjero.service.ts
@@ -1,29 +1,47 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { JwtHelperService } from '@auth0/angular-jwt';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GranjeroService {
-//url API base
-BASEAPI: string = 'http://localhost:3000/api/v1/';
-FARMERAPI: string = 'farmer/';
-LOGIN: string = 'login/'
+  //url API base
+  BASEAPI: string = 'http://localhost:3000/api/v1/';
+  FARMERAPI: string = 'farmer/';
+  LOGIN: string = 'login/'
   constructor(
     private http: HttpClient,
     private jwtHelper: JwtHelperService,
   ) { }
   //metodo para loguin granjero
-  getLoginFarmer(gra_cedula:any, gra_password:any) {
+  getLoginFarmer(gra_cedula: any, gra_password: any) {
     const body = { gra_cedula, gra_password };
     return this.http.post(this.BASEAPI + this.FARMERAPI + this.LOGIN, body);
   }
-  isLogin():boolean{
+  isLogin(): boolean {
     const token = localStorage.getItem('token');
-    if(this.jwtHelper.isTokenExpired(token) || !localStorage.getItem('token')){
+    if (this.jwtHelper.isTokenExpired(token) || !localStorage.getItem('token')) {
       return false;
     }
     return true;
+  }
+
+  //Listar granjeros
+  getFarmer() {
+    return this.http.get(this.BASEAPI + this.FARMERAPI);
+  }
+  //Agregar granjero
+  addFarmer(farmer: any): Observable<any> {
+    return this.http.post(this.BASEAPI + this.FARMERAPI, farmer);
+  }
+  //Editar granjero
+  editFarmer(gra_cedula: any, farmer: any): Observable<any> {
+    return this.http.patch(this.BASEAPI + this.FARMERAPI + gra_cedula, farmer);
+  }
+  //Eliminar granjero
+  deleteFarmer(gra_cedula: any): Observable<any> {
+    return this.http.delete(this.BASEAPI + this.FARMERAPI + gra_cedula);
   }
 }
