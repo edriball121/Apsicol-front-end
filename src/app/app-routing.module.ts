baa55b2b048fffe85e2d290a06ed211e86f5ab9b
edriball121 <53952759+edriball121@users.noreply.github.com>
2023-09-10 20:39:32 -0500

    Se agrega el crud de ciudad
    Se agrega el crud de ciudad

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,6 +13,7 @@ import { ConsultaComponent } from './components/consulta/consulta.component';
 import { GranjeroComponent } from './components/granjero/granjero.component';
 import { NoticiasComponent } from './components/noticias/noticias.component';
 import { ConsultorComponent } from './components/consultor/consultor.component';
+import { CiudadComponent } from './components/ciudad/ciudad.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'landingPage' },
@@ -24,7 +25,8 @@ const routes: Routes = [
   { path: 'consult', component: ConsultaComponent, data: { expectedRole: ['consultant', 'admin', 'farmer'] }, canActivate: [RoleGuard, AuthGuard] },
   { path: 'farmer', component: GranjeroComponent, data: { expectedRole: 'admin' }, canActivate: [RoleGuard, AuthGuard] },
   { path: 'news', component: NoticiasComponent, data: { expectedRole: 'admin' }, canActivate: [RoleGuard, AuthGuard] },
-  { path: 'consultant', component: ConsultorComponent},
+  { path: 'consultant', component: ConsultorComponent },
+  { path: 'city', component: CiudadComponent },
   //Not Found
   { path: '**', pathMatch: 'full', component: PageNotFoundComponent },
 ];
